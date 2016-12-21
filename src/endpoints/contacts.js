32f68064122e2bc0bee18ec49033f9ba05f3aabf
Henrique Schreiner <hms.sud@gmail.com>
2016-12-21 18:18:49 -0200

    Added error handler function.

--- src/endpoints/contacts.js
+++ src/endpoints/contacts.js
@@ -1,40 +1,25 @@
-'use strict'
+import errorHandler from '../helpers/errorHandler'
 
-import Request from '../helpers/request'
+class Contacts {
 
-const Contacts = (api) => {
+  constructor(api = null) {
 
-  return {
+    if (api === null) throw new Error('Request instance must be provided on constructor.')
 
-    /**
-     * Return all contacts
-     */
-    getAll: (params) => {
+    this.api = api
+  }
 
-      let defaultParams = {
-        count: null,
-        offset: null,
-        ...params
-      }
+  getAll(params) {
 
-      return api.get('contacts/v1/lists/all/contacts/all', defaultParams)
-        .then(response => response)
-        .catch(error => {
-
-          if (error.response) {
-              // The request was made, but the server responded with a status code
-              // that falls out of the range of 2xx
-              // console.log(error.response.data.message);
-              // console.log(error.response.status);
-              // console.log(error.response.headers);
-              throw new Error(error.response.data.message)
-            } else {
-              // Something happened in setting up the request that triggered an Error
-              throw new Error(error.message)
-            }
-            // console.log(error.config);
-        })
+    let defaultParams = {
+      count: null,
+      vidOffset: null,
+      ...params
     }
+
+    return this.api.get('contacts/v1/lists/all/contacts/all', defaultParams)
+      .then(response => response)
+      .catch(error => errorHandler(error))
   }
 }
 
