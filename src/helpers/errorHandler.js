8dc57e04f62ae2eaa6523f9987f9dd6220c9cb60
Henrique Schreiner <hms.sud@gmail.com>
2016-12-21 14:19:49 -0200

    Added a error handler helper file.

---
+++ src/helpers/errorHandler.js
@@ -0,0 +1,18 @@
+module.exports = (error) => {
+
+  if (error.response) {
+      // The request was made, but the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(`
+        ======> Error
+        Message: ${error.response.data.message}
+        Status: ${error.response.status}
+        headers: ${JSON.stringify(error.response.headers)}
+      `)
+
+      throw new Error(error.response.data.message)
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      throw new Error(error.message)
+    }
+}
