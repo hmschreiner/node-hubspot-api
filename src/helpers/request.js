cc6390c9c16911f3a16f33daa3d7a84685cd809a
Henrique Schreiner <hms.sud@gmail.com>
2016-12-21 18:20:16 -0200

    Changed function to ES6 class syntax.

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -2,21 +2,55 @@
 
 import axios from 'axios'
 
-const hbEndPoint = 'https://api.hubapi.com'
+const API_ENDPOINT = 'https://api.hubapi.com'
 
-const Request = (apiKey) => {
+class Request {
 
-  let apiInstance = axios.create({
-    baseURL: `${hbEndPoint}`,
-    timeout: 10000,
-  })
+  constructor(apiKey = null) {
 
-  return {
-    get: (endPoint, params = {}) => apiInstance.get(`${endPoint}?hapikey=${apiKey}`, { params: params }),
-    post: () => {},
-    put: () => {},
-    delete: () => {},
+    if (apiKey === null) throw new Error('You must provide the API key.')
+
+    this.apiKey = apiKey
+    this.apiInstance = axios.create({
+      baseURL: `${API_ENDPOINT}`,
+      timeout: 10000,
+    })
+  }
+
+  normalizeEndPointURL(endPoint) {
+      return `${endPoint}?hapikey=${this.apiKey}`
+  }
+
+  get(endPoint, params = {}) {
+    return this.apiInstance.get(this.normalizeEndPointURL(endPoint), { params: params })
+  }
+
+  post() {
+
+  }
+
+  put() {
+
+  }
+
+  delete() {
+
   }
 }
+
+// const Request = (apiKey) => {
+//
+//   let apiInstance = axios.create({
+//     baseURL: `${hbEndPoint}`,
+//     timeout: 10000,
+//   })
+//
+//   return {
+//     get: (endPoint, params = {}) => apiInstance.get(`${endPoint}?hapikey=${apiKey}`, { params: params }),
+//     post: () => {},
+//     put: () => {},
+//     delete: () => {},
+//   }
+// }
 
 export default Request
