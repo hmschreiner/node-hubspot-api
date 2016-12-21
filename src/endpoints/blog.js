e6bf5ab62e413d0a8dda190eb52c8b0b42fb59ed
Henrique Schreiner <hms.sud@gmail.com>
2016-12-21 18:22:43 -0200

    Added blog endpoint.

---
+++ src/endpoints/blog.js
@@ -0,0 +1,48 @@
+import errorHandler from '../helpers/errorHandler'
+
+class Blog {
+
+  constructor(api = null) {
+
+    if (api === null) throw new Error('Request instance must be provided on constructor.')
+
+    this.api = api
+  }
+
+  getAllBlogs(params) {
+
+    return this.api.get('content/api/v2/blogs', { ...params })
+      .then(response => response)
+      .catch(error => errorHandler(error))
+  }
+
+  getPosts(params) {
+
+    return this.api.get('content/api/v2/blog-posts', { ...params })
+      .then(response => response)
+      .catch(error => errorHandler(error))
+  }
+
+  getPostBySlug(slug) {
+
+    return this.api.get('content/api/v2/blog-posts', { slug })
+      .then(response => response)
+      .catch(error => errorHandler(error))
+  }
+
+  getPostById(postId) {
+
+    return this.api.get(`content/api/v2/blog-posts/${postId}`)
+      .then(response => response)
+      .catch(error => errorHandler(error))
+  }
+
+  getAllTopics(params) {
+
+    return this.api.get('content/api/v2/blog-posts', { ...params })
+      .then(response => response)
+      .catch(error => errorHandler(error))
+  }
+}
+
+module.exports = Blog
---
+++ test/blog.js
@@ -0,0 +1,66 @@
+import chai from 'chai'
+import NodeHubspotApi from '../src'
+import { apiKey } from './config'
+
+let expect = chai.expect
+
+const api = new NodeHubspotApi(apiKey)
+
+describe('Blog', () => {
+
+  describe('Get blogs', () => {
+
+    it('Should return all blogs', done => {
+
+      api.blog.getAllBlogs(api)
+        .then(response => {
+          expect(response.status).to.equal(200)
+          expect(response.data).to.be.a('object')
+          expect(response.data.objects).to.be.a('array')
+          done()
+        })
+        .catch(error => done(error))
+    })
+  })
+
+  describe('Get blog posts', () => {
+
+    let content_group_id = 351076997
+    let blogPostId       = 3198892953
+
+    it('Should return all blog posts', done => {
+
+      api.blog.getPosts()
+        .then(response => {
+          expect(response.status).to.equal(200)
+          expect(response.data).to.be.a('object')
+          expect(response.data.objects).to.be.a('array')
+          done()
+        })
+        .catch(error => done(error))
+    })
+
+    it(`Should return all posts from blog with content_group_id = ${content_group_id}`, done => {
+
+      api.blog.getPosts({content_group_id})
+        .then(response => {
+          expect(response.status).to.equal(200)
+          expect(response.data).to.be.a('object')
+          expect(response.data.objects).to.be.a('array')
+          done()
+        })
+        .catch(error => done(error))
+    })
+
+    it(`Should return a blog post with blog_post_id = ${blogPostId}`, done => {
+
+      api.blog.getPostById(blogPostId)
+        .then(response => {
+          expect(response.status).to.equal(200)
+          expect(response.data).to.be.a('object')
+          expect(response.data.html_title).to.equal('GPI First API blog post!')
+        })
+        .catch(error => done(error))
+    })
+  })
+})
