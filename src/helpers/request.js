9c57514d73d9980ccfded6d88e4fa3c7045bf56d
Henrique Schreiner <hms.sud@gmail.com>
2016-12-29 12:03:14 -0200

    Changed to use request-promise to HTTP requests.

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import rp from 'request-promise'
 
 const API_ENDPOINT = 'https://api.hubapi.com'
 
@@ -9,18 +9,23 @@ class Request {
     if (apiKey === null) throw new Error('You must provide the API key.')
 
     this.apiKey = apiKey
-    this.apiInstance = axios.create({
-      baseURL: `${API_ENDPOINT}`,
-      timeout: 10000,
-    })
+    this.apiOptions = {
+      host: `${API_ENDPOINT}`,
+    };
   }
 
   normalizeEndPointURL(endPoint) {
-      return `${endPoint}?hapikey=${this.apiKey}`
+      return `${API_ENDPOINT}/${endPoint}?hapikey=${this.apiKey}`
   }
 
   get(endPoint, params = {}) {
-    return this.apiInstance.get(this.normalizeEndPointURL(endPoint), { params })
+
+    return rp({
+      uri: this.normalizeEndPointURL(endPoint),
+      qs: params,
+      json: true,
+      resolveWithFullResponse: true,
+    })
   }
 
   // TODO
