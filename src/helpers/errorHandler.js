f9583ca4523ac7b7c98ec79bb4153105815ae3a1
Henrique Schreiner <hms.sud@gmail.com>
2016-12-31 15:08:17 -0200

    Fixed error handler to deal with different responses from API

--- src/helpers/errorHandler.js
+++ src/helpers/errorHandler.js
@@ -5,13 +5,13 @@ module.exports = (error) => {
       // that falls out of the range of 2xx
       console.log(`
         ======> Error
-        Message: ${error.response.data.message}
+        Message: ${error.response.data.message || error.response.statusText}
         Status Code: ${error.response.status}
-        Status: ${error.response.data.status}
+        Status: ${error.response.data.status || ''}
         headers: ${JSON.stringify(error.response.headers)}
       `)
 
-      throw new Error(error.response.data.message)
+      throw new Error(error.response.data.message || error.response.statusText)
     } else {
       // Something happened in setting up the request that triggered an Error
       throw new Error(error)
