cf439a8fd58ba5609b56263b3d52bad736629391
Henrique Schreiner <hms.sud@gmail.com>
2017-06-13 13:59:24 -0300

    Removed console logs for response handlers.

--- src/helpers/errorHandler.js
+++ src/helpers/errorHandler.js
@@ -1,16 +1,9 @@
 module.exports = (error) => {
 
   if (error.response) {
+
       // The request was made, but the server responded with a status code
       // that falls out of the range of 2xx
-      console.log(`
-        ======> Error
-        Message: ${error.response.data.message || error.response.statusText}
-        Status Code: ${error.response.status}
-        Status: ${error.response.data.status || ''}
-        headers: ${JSON.stringify(error.response.headers)}
-      `)
-
       throw new Error(error.response.data.message || error.response.statusText)
     } else {
       // Something happened in setting up the request that triggered an Error
--- src/helpers/responseHandler.js
+++ src/helpers/responseHandler.js
@@ -1,15 +1,9 @@
 module.exports = (response) => {
 
   if (response.statusText === 'error') {
+    
     // The request was made, but the server responded with a status code
     // that falls out of the range of 2xx
-    console.log(`
-      ======> Error
-      Message: ${response.body.message}
-      Status: ${error.statusCode}
-      headers: ${JSON.stringify(error.headers)}
-    `)
-
     throw new Error(response.body.message)
   }
 
